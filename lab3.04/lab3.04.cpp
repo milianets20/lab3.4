4743dc8de43f55ca19207e985a31bfebfc995005
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-10-20 10:35:40 +0300

    srcond m

---
+++ lab3.04/lab3.04.cpp
@@ -0,0 +1,24 @@
+﻿// Lab_03_4.cpp
+// < прізвище, ім’я автора >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 20
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	// розгалуження в повній формі
+	if ((y >= (x - 2) * (x - 2) - 3) && (y > 0) && (y < x) ||
+		(y >= (x - 2) * (x - 2) - 3) && (y < fabs(x)))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
